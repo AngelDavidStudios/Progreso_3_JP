b0cdf9ef7b38871449452a329111103c311b40e5
Angel David <angeldavidstudios@outlook.com>
2024-06-30 22:22:20 -0500

    Inicio del proyecto

---
+++ TestAPI_Demo_P3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestAPI_Demo_P3;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ TestAPI_Demo_P3/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TestAPI_Demo_P3;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
